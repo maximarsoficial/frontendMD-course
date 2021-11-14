1ebb1fa3ad882b0957e283acb056c5c1e339ad53
Maximiliano Mercado Diaz <50929240+maximarsoficial@users.noreply.github.com>
2021-11-14 13:59:39 -0300

    tarea ejercicio autos
    ejer 1 listo y ejer 2 en proceso

--- frontend/ejercicios-autos-js/ejercicio-1.js
+++ frontend/ejercicios-autos-js/ejercicio-1.js
@@ -1,6 +1,32 @@
 
-    // 1) Jorge tiene una consecionaria de autos, y desea mostrar en su página web el listado de autos que tiene disponibles.
+    // 1) Jorge tiene una concesionaria de autos, y desea mostrar en su página web el listado de autos que tiene disponibles.
     //     - Crear una función que tome un listado de autos e imprima por consola el modelo y la marca de *cada auto* con la leyenda `Tenemos disponible un ${modelo}`. Utilizar el método `for` o `forEach` para recorrer el array.
     //     - Ejecutar dicha función pasando por parámetros el array autosDisponibles.
 
-    
+    const autosDisponibles = [
+        {marca: 'Chevrolet', modelo: 'Camaro', puertas: 2},
+        {marca: 'Chevrolet', modelo: 'Cruze', puertas: 4},
+        {marca: 'Ford', modelo: 'Fiesta', puertas: 4},
+        {marca: 'Ford', modelo: 'Focus', puertas: 4},
+        {marca: 'Ford', modelo: 'Mustang', puertas: 4},
+        {marca: 'Ford', modelo: 'Fusion', puertas: 4},
+        {marca: 'Ford', modelo: 'Ka', puertas: 2},
+        {marca: 'Audi', modelo: 'A4', puertas: 5},
+        {marca: 'Audi', modelo: 'A5', puertas: 5},
+        {marca: 'Audi', modelo: 'A6', puertas: 5},
+        {marca: 'Audi', modelo: 'A7', puertas: 5},
+        {marca: 'Audi', modelo: 'A8', puertas: 5},
+        {marca: 'Audi', modelo: 'Q5', puertas: 5},
+        {marca: 'BMW', modelo: 'Serie 3', puertas: 5},
+        {marca: 'BMW', modelo: 'Serie 5', puertas: 5},
+        {marca: 'BMW', modelo: 'Serie 7', puertas: 5},
+        {marca: 'Mercedes Benz', modelo: 'Clase A', puertas: 5},
+        {marca: 'Mercedes Benz', modelo: 'Clase C', puertas: 5},
+        {marca: 'Mercedes Benz', modelo: 'Clase E', puertas: 5},
+    ];
+
+   autosDisponibles.forEach(element => {
+    console.log('TENEMOS DISPONIBLES EL AUTO - MODELO:',element.modelo,' --> ', ' MARCA:',element.marca)
+   });
+
+ 
--- frontend/ejercicios-autos-js/ejercicio-2.js
+++ frontend/ejercicios-autos-js/ejercicio-2.js
@@ -5,3 +5,33 @@
 //         -Verificar que vehículos cumplen la condición de puertas requeridas.
 //         -Ejecutar la función, devolviendo el resultado a una nueva lista llamada autosPuertaGenaro. Utilizaremos el método map() para recorrer el array de autosDisponibles y devolver una nueva lista con los autos que cumplen la condición.
 //         -Imprimir autosPuertaGenaro utilizando la funcion del punto (1).
+
+const autosDisponibles = [
+    {marca: 'Chevrolet', modelo: 'Camaro', puertas: 2},
+    {marca: 'Chevrolet', modelo: 'Cruze', puertas: 4},
+    {marca: 'Ford', modelo: 'Fiesta', puertas: 4},
+    {marca: 'Ford', modelo: 'Focus', puertas: 4},
+    {marca: 'Ford', modelo: 'Mustang', puertas: 4},
+    {marca: 'Ford', modelo: 'Fusion', puertas: 4},
+    {marca: 'Ford', modelo: 'Ka', puertas: 2},
+    {marca: 'Audi', modelo: 'A4', puertas: 5},
+    {marca: 'Audi', modelo: 'A5', puertas: 5},
+    {marca: 'Audi', modelo: 'A6', puertas: 5},
+    {marca: 'Audi', modelo: 'A7', puertas: 5},
+    {marca: 'Audi', modelo: 'A8', puertas: 5},
+    {marca: 'Audi', modelo: 'Q5', puertas: 5},
+    {marca: 'BMW', modelo: 'Serie 3', puertas: 5},
+    {marca: 'BMW', modelo: 'Serie 5', puertas: 5},
+    {marca: 'BMW', modelo: 'Serie 7', puertas: 5},
+    {marca: 'Mercedes Benz', modelo: 'Clase A', puertas: 5},
+    {marca: 'Mercedes Benz', modelo: 'Clase C', puertas: 5},
+    {marca: 'Mercedes Benz', modelo: 'Clase E', puertas: 5},
+];
+
+const autosPuertaGenaro = autosDisponibles.map(element => {
+    if(element.puertas <=4)
+    console.log('TENEMOS DISPONIBLES EL AUTO - MODELO:',element.modelo,' --> ', ' MARCA:',element.marca)
+    return element.puertas <= 4 ;
+})
+
+console.log('La cantidad de autos con 4 o menos puertas son:', autosPuertaGenaro)
